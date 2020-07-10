b0b1b2f6902e779171753cc61d7a7e20b7f62457
Lucas <lucaslopes05597@gmail.com>
2020-07-09 21:14:37 -0300

    Tela Play

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,12 +1,73 @@
 import React from "react";
-import { View } from "react-native";
-
-import { Container } from "./styles";
+import SoundCoverImage from "../../../assets/SoundCoverImage.png";
+import PlayButton from "../../../assets/Play.png";
+import { Image } from "react-native";
+import {
+  Container,
+  ScreenArea,
+  CoverArea,
+  PlayerArea,
+  Img,
+  PlayerTitle,
+  PlayerAuthor,
+  PlayerAreaButtons,
+  ButtonPlay,
+  BackButton,
+  NextButton,
+  PlayerSlider,
+  AudioSlider,
+  TimeStart,
+  TimeEnd,
+  PlayerTime
+} from "./styles";
 import TopBar from "../../components/TopBar";
+import Back from "../../../assets/Back.png";
+import Next from "../../../assets/Next.png";
 const Play = () => {
   return (
     <Container>
       <TopBar />
+
+      <ScreenArea>
+        <CoverArea>
+          <Img>
+            <Image source={SoundCoverImage} />
+          </Img>
+        </CoverArea>
+
+        <PlayerArea>
+          <PlayerTitle>Porque usar React #130</PlayerTitle>
+          <PlayerAuthor>Lucas Lopes</PlayerAuthor>
+
+          <PlayerSlider>
+            <AudioSlider
+              thumbTintColor="#FFFFFF"
+              minimumTrackTintColor="#1DD65F"
+              maximumTrackTintColor="#777777"
+              minimumValue={0}
+              maximumValue={100}
+            />
+          </PlayerSlider>
+          <PlayerTime>
+            <TimeStart>0:00</TimeStart>
+            <TimeEnd>15:00</TimeEnd>
+          </PlayerTime>
+
+          <PlayerAreaButtons>
+            <BackButton>
+              <Image source={Back} />
+            </BackButton>
+
+            <ButtonPlay>
+              <Image source={PlayButton} />
+            </ButtonPlay>
+
+            <NextButton>
+              <Image source={Next} />
+            </NextButton>
+          </PlayerAreaButtons>
+        </PlayerArea>
+      </ScreenArea>
     </Container>
   );
 };
--- src/pages/Play/styles.js
+++ src/pages/Play/styles.js
@@ -1,6 +1,68 @@
 import styled from "styled-components/native";
-
+import { Slider } from "react-native";
 export const Container = styled.View`
   background: #1b1a1f;
   flex: 1;
+`;
+
+export const ScreenArea = styled.View`
+  flex: 1;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 32px;
+`;
+export const CoverArea = styled.View`
+  flex: 2;
+`;
+
+export const Img = styled.View`
+  margin-left: 50px;
+  margin-top: 50px;
+`;
+export const PlayerArea = styled.View`
+  flex: 1;
+`;
+
+export const PlayerTitle = styled.Text`
+  color: #ffff;
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+export const PlayerAuthor = styled.Text`
+  color: #bbb;
+  font-size: 12px;
+`;
+
+export const PlayerAreaButtons = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const NextButton = styled.TouchableOpacity``;
+export const BackButton = styled.TouchableOpacity``;
+export const ButtonPlay = styled.TouchableOpacity`
+  padding: 0 25px;
+`;
+
+export const PlayerSlider = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const AudioSlider = styled(Slider)`
+  margin-top: 15px;
+  flex-basis: 100%;
+`;
+
+export const PlayerTime = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 10px;
+`;
+
+export const TimeStart = styled.Text`
+  color: #eee;
+`;
+export const TimeEnd = styled.Text`
+  color: #eee;
 `;
